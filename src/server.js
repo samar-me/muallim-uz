75f7fc0e03de482d89d9cfcd53242e9e51a968dc
Muallim Developer <dev@muallim.uz>
2026-09-13 02:08:45 -0700

    fix: route static images via serverless endpoint in vercel.json

--- src/server.js
+++ src/server.js
@@ -232,6 +232,14 @@ app.post('/api/telegram-webhook', async (req, res) => {
   }
 });
 
+// Statik rasmlarni serverless orqali to'g'ridan-to'g'ri berish
+app.get('/muallim_logo.jpg', (req, res) => {
+  res.sendFile(path.join(publicDir, 'muallim_logo.jpg'));
+});
+app.get('/muallim_banner.jpg', (req, res) => {
+  res.sendFile(path.join(publicDir, 'muallim_banner.jpg'));
+});
+
 // Bosh sahifa (index.html)
 app.get('/', (req, res) => {
   res.sendFile(path.join(publicDir, 'index.html'));
